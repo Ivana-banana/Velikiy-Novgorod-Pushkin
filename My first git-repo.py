d124db34c9c39e3540feeeb42f3d694b523d945a
Student <student@student.com>
2023-11-17 19:08:21 +0300

    третий коммит

--- My first git-repo.py
+++ My first git-repo.py
@@ -1,1 +1,37 @@
-print('My first git-repo')
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.label.setText("OK")
+        self.second_form.show()
+
+    def open_second_form(self):
+        self.second_form = SecondForm(self, "Данные для второй формы")
+        self.second_form.show()
+
+
+class SecondForm(QWidget):
+    def __init__(self, *args):
+        super().__init__()
+        uic.loadUi('two.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.initUI(args)
+
+    def initUI(self, args):
+        self.label = QLabel(args[-1], self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
